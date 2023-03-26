6d8a1e432283a62d2ebcde3247d64f78e02dddba
Neo <neoaichan@gmail.com>
2023-03-26 23:22:05 +0000

    Many changes.

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hi from Neo!");
+function showSection(evt, sectionName) {
+  var i, articleItem;
+  articleItem = document.getElementsByClassName("article-item");
+  for (i = 0; i < articleItem.length; i++) {
+    articleItem[i].style.display = "none";
+  }
+  document.getElementById(sectionName).style.display = "block";
+
+  // Remove the "active" class from all buttons
+  var buttons = document.getElementsByClassName("article-buttons");
+  for (i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("active");
+  }
+
+  // Add the "active" class to the clicked button
+  evt.currentTarget.classList.add("active");
+}
